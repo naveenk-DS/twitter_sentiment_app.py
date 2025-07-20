4b14d934caeab69271f62a562d106be93a594b46
Naveen K <jaguarff66@gmail.com>
2025-07-20 09:33:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,19 @@ st.title("Twitter Sentiment Analysis")
 tweet = st.text_area("Enter a Tweet")
 
 if st.button("Analyze"):
-    # For now, just a dummy result
-    st.write("Predicted Sentiment: Positive 😊")
+   if tweet.strip() == "":
+        st.warning("Please enter a tweet.")
+    else:
+        cleaned_tweet = preprocess_tweet(tweet)
+        vectorized_tweet = vectorizer.transform([cleaned_tweet])
+        prediction = model.predict(vectorized_tweet)[0]
+
+        # Optional: mapping numeric to text if needed
+        if prediction == 0:
+            sentiment = "Negative 😠"
+        elif prediction == 1:
+            sentiment = "Neutral 😐"
+        else:
+            sentiment = "Positive 😊"
+
+        st.write(f"**Predicted Sentiment:** {sentiment}")
